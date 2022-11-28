5a872c346d279f0650eb72742caca439c10249d0
josem <josem@DESKTOP-Q9T0N5L>
2022-11-27 21:34:30 -0300

    Se agrega Pantalla Medico Web.

--- TP_Final_Morales_Rangogni/MedicoWeb.aspx.cs
+++ TP_Final_Morales_Rangogni/MedicoWeb.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using AccesoModeloBaseDatos.Dominio;
+using ModeloDeNegocio;
 using ModeloDeNegocio.Negocio;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Web;
@@ -26,7 +28,9 @@ namespace TP_Final_Morales_Rangogni
                 }
                 if (!IsPostBack)
                 {
-                    CargarGrillaEspecialidad();
+                    CargarFecha();
+                    CargarGrillaTurnos();
+
                 }
             }
             catch (Exception ex)
@@ -50,28 +54,6 @@ namespace TP_Final_Morales_Rangogni
             }
         }
 
-        private void CargarGrillaEspecialidad()
-        {
-            try
-            {
-                EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
-                Empleado empleado = (Empleado)Session["EmpleadoLogin"];
-
-                List<Especialidad> especialidades = null;
-                if (empleado.idPerfil== 1)
-                    especialidades = especialidadNegocio.Especialidades();
-                if (empleado.idPerfil == 3)
-                    especialidades = especialidadNegocio.MedicoEspecialidades(empleado.ID);
-                dgvEspecialidad.DataSource = especialidades;
-                dgvEspecialidad.DataBind();
-                Session.Add("ListaEspecialidad", especialidades);
-            }
-            catch (Exception ex)
-            {
-                Session.Add("MensajeError", ex.ToString());
-                Response.Redirect("ErrorWeb.aspx", false);
-            }
-        }
 
         protected void dgvMedicos_RowCommand(object sender, GridViewCommandEventArgs e)
         {
@@ -85,13 +67,11 @@ namespace TP_Final_Morales_Rangogni
                 ModeloTurnoWeb modeloTurnoWeb = modeloTurnos.Find(x => x.IdTurno.Equals(idJorn));
                 if (modeloTurnoWeb == null)
                     throw new Exception("Fallo al seleccionar el truno para editar");
-                ///txtId.Text = modeloTurnoWeb.IdTurno.ToString();
-               // txtPaciente.Text = modeloTurnoWeb.NombrePaciente.ToString();
-                //txtMedico.Text = modeloTurnoWeb.NombreMedico.ToString();
-                //txtDia.Text = modeloTurnoWeb.FechaReserva.ToString();
-                //txtHora.Text = modeloTurnoWeb.Hora.ToString();
-                //txtObservacion.Text = modeloTurnoWeb.Observacion.ToString();
-                //ddlSituacion.SelectedValue = modeloTurnoWeb.IdSituacion.ToString();
+                txtId.Text = modeloTurnoWeb.IdTurno.ToString();
+                txtPaciente.Text = modeloTurnoWeb.NombrePaciente.ToString();
+                txtDia.Text = modeloTurnoWeb.FechaReserva.ToString();
+                txtHora.Text = modeloTurnoWeb.Hora.ToString();
+                txtObservacion.Text = modeloTurnoWeb.Observacion.ToString();
                 mpe.Show();
             }
             catch (Exception ex)
@@ -106,41 +86,6 @@ namespace TP_Final_Morales_Rangogni
             mvwMedico.ActiveViewIndex = index;
         }
 
-        protected void dgvEspecialidad_RowCommand(object sender, GridViewCommandEventArgs e)
-        {
-            try
-            {
-                List<Especialidad> especialidades = (List<Especialidad>)Session["ListaEspecialidad"];
-                int numRow = Convert.ToInt32(e.CommandArgument);
-                GridView gridView = (GridView)sender;
-                int id = int.Parse(gridView.Rows[numRow].Cells[0].Text.ToString());
-                Especialidad especialidad = especialidades.Find(x => x.IdEspecialidad.Equals(id));
-                txtIdEspMed.Text = especialidad.IdEspecialidad.ToString();
-                txtIdEspMed.Enabled = false;
-                Empleado empleado = (Empleado)Session["EmpleadoLogin"];
-                txtIdMed.Text = empleado.ID.ToString();
-                lblAccionEsp.Text = e.CommandName.ToUpper();
-                if (e.CommandName.Equals("Editar"))
-                {
-                    ActivaDesactivaControlesModal(true);
-                    ddlEspecialidad.Items.Clear();
-                    ddlEspecialidad.Items.Add(especialidad.Descripcion);
-                    chkEstMedEsp.Checked = especialidad.Estado.Equals("Activo") ? true : false;
-                }
-                mpeEsp.Show();
-            }
-            catch (Exception ex)
-            {
-                Session.Add("MensajeError", ex.ToString());
-                Response.Redirect("ErrorWeb.aspx", false);
-            }
-        }
-
-        private void ActivaDesactivaControlesModal(bool est)
-        {
-            ddlEspecialidad.Enabled= est;
-        }
-
         protected void lbtnModal_Click(object sender, EventArgs e)
         {
 
@@ -153,42 +98,28 @@ namespace TP_Final_Morales_Rangogni
 
         private void LimpiarControles()
         {
-            txtDesc.Text = "";
-            txtId.Text = "";
-            lblAccion.Text = "";
-        }
 
-        protected void lbtnNuevaEsp_Click(object sender, EventArgs e)
-        {
-            LimpiarControles();
-            Empleado empleado = (Empleado)Session["EmpleadoLogin"];
-            txtIdMed.Text = empleado.ID.ToString();
-            txtIdMed.Enabled = false;
-            CargarDropDawnListEspecialidad(true);
-            chkEstMedEsp.Checked = true;
-            lblAccionEsp.Text = "NUEVO";
-            mpeEsp.Show();
         }
-
-        private void CargarDropDawnListEspecialidad(bool nuevo)
+        private void CargarGrillaTurnos()
         {
             try
             {
-                EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
-                List<Especialidad> especialidades = null;
-                if (nuevo)
-                    especialidades = especialidadNegocio.Especialidades();
+                TurnoNegocio turnoNegocio = new TurnoNegocio();
+                ModeloGrillaTurnosWeb modeloGrillaTurnosWeb = new ModeloGrillaTurnosWeb();
+                DateTime dFechaTurno;
+                if (!IsPostBack || txtFechaTurnos.Text.Equals(""))
+                    dFechaTurno = DateTime.Today;
                 else
-                    especialidades = (List<Especialidad>)Session["ListaEspecialidad"];
+                    dFechaTurno = Convert.ToDateTime(txtFechaTurnos.Text);
+                DataTable dtTurnos = turnoNegocio.DataTableTurnosFecha(dFechaTurno);
+                List<ModeloTurnoWeb> gvTurnos = modeloGrillaTurnosWeb.ObtenerListaTurnosWebDataTable(dtTurnos);
 
-                ddlEspecialidad.DataSource = especialidades;
-                ddlEspecialidad.DataValueField = "IdEspecialidad";
-                ddlEspecialidad.DataTextField = "Descripcion";
-                ddlEspecialidad.DataBind();
-                ddlEspecialidad.Items.Insert(0, new ListItem("-- Seleccione --", "0"));
-                ddlEspecialidad.SelectedIndex = 0;
-                txtIdEspMed.Text = ddlEspecialidad.SelectedValue;
-                txtIdEspMed.Enabled = false;
+                if (Session["TurnosGrdWeb"] != null)
+                    Session.Remove("TurnosGrdWeb");
+
+                Session.Add("TurnosGrdWeb", gvTurnos);
+                dgvMedicos.DataSource = gvTurnos;
+                dgvMedicos.DataBind();
             }
             catch (Exception ex)
             {
@@ -196,64 +127,35 @@ namespace TP_Final_Morales_Rangogni
                 Response.Redirect("ErrorWeb.aspx", false);
             }
         }
-
-        protected void lbtnCargarEsp_Click(object sender, EventArgs e)
+        private void FiltrarGrillaTurnos()
         {
-            CargarGrillaEspecialidad();
-        }
+            string paciente = txtfiltroPaciente.Text.Trim();
+            string dni = txtFiltroDni.Text.Trim();
 
-        protected void lbtnGrabaEsp_Click(object sender, EventArgs e)
-        {
-            if (lblAccionEsp.Text.Equals("NUEVO"))
-                AltaMedicoEspecialidadWeb();
-            if (lblAccionEsp.Text.Equals("EDITAR"))
-                ModificarMedicoEspeciaalidadWeb();
-        }
-
-        private void AltaMedicoEspecialidadWeb()
-        {
-            if (txtIdEspMed.Text.Trim().Equals(""))
+            if (Session["TurnosGrdWeb"] == null)
                 return;
-            EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
-            try
-            {
-                Empleado empleado = (Empleado)Session["EmpleadoLogin"];
-                if (empleado.idPerfil != 3)
-                    return;
-                especialidadNegocio.AltaMedicoEspecialidad(empleado.ID, Convert.ToInt32(txtIdEspMed.Text), chbEst.Checked);
-            } 
-            catch (Exception ex)
-            {
-                Session.Add("MensajeError", ex.ToString());
-                Response.Redirect("ErrorWeb.aspx", false);
-            }
-            CargarGrillaEspecialidad();
+
+            List<ModeloTurnoWeb> modeloTurnosWeb = (List<ModeloTurnoWeb>)Session["TurnosGrdWeb"];
+            List<ModeloTurnoWeb> modeloTurnosFiltro = modeloTurnosWeb;
+            if (!dni.Equals(""))
+                modeloTurnosFiltro = modeloTurnosFiltro.FindAll(x => x.NombreMedico.ToUpper().Contains(dni.ToUpper()));
+            if (!paciente.Equals(""))
+                modeloTurnosFiltro = modeloTurnosFiltro.FindAll(x => x.NombrePaciente.ToUpper().Contains(paciente.ToUpper()));
+            dgvMedicos.DataSource = modeloTurnosFiltro;
+            dgvMedicos.DataBind();
         }
 
-        private void ModificarMedicoEspeciaalidadWeb()
+        protected void lbtnCargaGrd_Click(object sender, EventArgs e)
         {
-            if (txtIdMed.Text.Trim().Equals(""))
-                return;
-            EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
-            try
-            {
-                Empleado empleado = (Empleado)Session["EmpleadoLogin"];
-                if (empleado.idPerfil != 3)
-                    return;
-                especialidadNegocio.ModificarMedicoEspecialidad(Convert.ToInt32(txtIdMed.Text),Convert.ToInt32(txtIdEspMed.Text), chbEst.Checked);
-            }
-            catch (Exception ex)
-            {
-                Session.Add("MensajeError", ex.ToString());
-                Response.Redirect("ErrorWeb.aspx", false);
-            }
-            CargarGrillaEspecialidad();
+            LinkButton button = (LinkButton)sender;
+            CargarGrillaTurnos();
+            if (button.ID.Equals("lbtnCargaGrd"))
+                FiltrarGrillaTurnos();
         }
-
-        protected void ddlEspecialidad_SelectedIndexChanged(object sender, EventArgs e)
+        private void CargarFecha()
         {
-            txtIdEspMed.Text = ddlEspecialidad.SelectedValue;
-            mpeEsp.Show();
+            txtFechaTurnos.Text = DateTime.Now.ToString("yyyy-MM-dd");
+
         }
     }
 }
--- TP_Final_Morales_Rangogni/MedicoWeb.aspx.designer.cs
+++ TP_Final_Morales_Rangogni/MedicoWeb.aspx.designer.cs
@@ -51,6 +51,60 @@ namespace TP_Final_Morales_Rangogni
         protected global::System.Web.UI.UpdatePanel upMedicos;
 
         /// <summary>
+        /// Control txtFechaTurnos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaTurnos;
+
+        /// <summary>
+        /// Control lblPacienteGrd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPacienteGrd;
+
+        /// <summary>
+        /// Control txtfiltroPaciente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtfiltroPaciente;
+
+        /// <summary>
+        /// Control lblDniGrd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDniGrd;
+
+        /// <summary>
+        /// Control txtFiltroDni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFiltroDni;
+
+        /// <summary>
+        /// Control lbtnCargaGrd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbtnCargaGrd;
+
+        /// <summary>
         /// Control dgvMedicos.
         /// </summary>
         /// <remarks>
@@ -60,31 +114,22 @@ namespace TP_Final_Morales_Rangogni
         protected global::System.Web.UI.WebControls.GridView dgvMedicos;
 
         /// <summary>
-        /// Control btnVerJornadas.
+        /// Control lbtnCargarTurnos.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnVerJornadas;
+        protected global::System.Web.UI.WebControls.LinkButton lbtnCargarTurnos;
 
         /// <summary>
-        /// Control lbtnModal.
+        /// Control btnModal.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbtnModal;
-
-        /// <summary>
-        /// Control ClientButton.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ClientButton;
+        protected global::System.Web.UI.WebControls.Button btnModal;
 
         /// <summary>
         /// Control ModalPanel.
@@ -105,40 +150,49 @@ namespace TP_Final_Morales_Rangogni
         protected global::System.Web.UI.WebControls.TextBox txtId;
 
         /// <summary>
-        /// Control txtDesc.
+        /// Control txtPaciente.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtDesc;
+        protected global::System.Web.UI.WebControls.TextBox txtPaciente;
 
         /// <summary>
-        /// Control txtIni.
+        /// Control txtDni.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtIni;
+        protected global::System.Web.UI.WebControls.TextBox txtDni;
 
         /// <summary>
-        /// Control txtFin.
+        /// Control txtDia.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtFin;
+        protected global::System.Web.UI.WebControls.TextBox txtDia;
 
         /// <summary>
-        /// Control chbEst.
+        /// Control txtHora.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox chbEst;
+        protected global::System.Web.UI.WebControls.TextBox txtHora;
+
+        /// <summary>
+        /// Control txtObservacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtObservacion;
 
         /// <summary>
         /// Control lblAccion.
@@ -175,149 +229,5 @@ namespace TP_Final_Morales_Rangogni
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::AjaxControlToolkit.ModalPopupExtender mpe;
-
-        /// <summary>
-        /// Control View1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.View View1;
-
-        /// <summary>
-        /// Control upEspecialidad.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel upEspecialidad;
-
-        /// <summary>
-        /// Control dgvEspecialidad.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView dgvEspecialidad;
-
-        /// <summary>
-        /// Control lbtnCargarEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbtnCargarEsp;
-
-        /// <summary>
-        /// Control lbtnNuevaEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbtnNuevaEsp;
-
-        /// <summary>
-        /// Control btnEspMed.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnEspMed;
-
-        /// <summary>
-        /// Control ModalPanelEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel ModalPanelEsp;
-
-        /// <summary>
-        /// Control txtIdMed.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtIdMed;
-
-        /// <summary>
-        /// Control txtIdEspMed.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtIdEspMed;
-
-        /// <summary>
-        /// Control lblEspecialidad.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEspecialidad;
-
-        /// <summary>
-        /// Control ddlEspecialidad.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlEspecialidad;
-
-        /// <summary>
-        /// Control chkEstMedEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox chkEstMedEsp;
-
-        /// <summary>
-        /// Control lblAccionEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblAccionEsp;
-
-        /// <summary>
-        /// Control lbtnGrabaEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbtnGrabaEsp;
-
-        /// <summary>
-        /// Control lbtnCancelEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbtnCancelEsp;
-
-        /// <summary>
-        /// Control mpeEsp.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::AjaxControlToolkit.ModalPopupExtender mpeEsp;
     }
 }
